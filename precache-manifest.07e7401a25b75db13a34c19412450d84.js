7855a96cb7766bba4e0291da0b682344647ccc18
Ubuntu <ubuntu@ip-172-31-35-79.eu-central-1.compute.internal>
2023-06-16 12:14:55 +0000

    Updates

---
+++ precache-manifest.07e7401a25b75db13a34c19412450d84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9de74a323241b7ad4896",
+    "url": "/yolojs/static/css/main.ea6cd586.chunk.css"
+  },
+  {
+    "revision": "9de74a323241b7ad4896",
+    "url": "/yolojs/static/js/main.bca66dce.chunk.js"
+  },
+  {
+    "revision": "e89f037fa4848cd16791",
+    "url": "/yolojs/static/js/runtime~main.c1059b8c.js"
+  },
+  {
+    "revision": "4a6aaa4d258d3f932bc7",
+    "url": "/yolojs/static/js/2.706ec904.chunk.js"
+  },
+  {
+    "revision": "c11f018bb4496eb948f76ba1751d7aed",
+    "url": "/yolojs/index.html"
+  }
+];
